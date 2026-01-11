0115798005d4546266c6956e31e7714d8d76908a
biyahdaan <biyahdaan@gmail.com>
2026-01-11 17:46:14 +0530

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,5 @@
+function openLogin() {
+    // Jab aap login page bana lenge, tab yahan uska link aayega
+    alert("Login Page jaldi hi banaya jayega!");
+    // window.location.href = "login.html"; 
+}
